232ce1cd0f1af82ebb318ef188b10a0fe67cfcdc
ae-user <angel.iglesias.prestamo@gmail.com>
2022-02-28 14:33:01 +0100

    creating a draft of the checkout

--- webapp/src/components/Checkout.tsx
+++ webapp/src/components/Checkout.tsx
@@ -2,11 +2,41 @@ import * as React from "react";
 
 import Container from "@mui/material/Container";
 import Paper from "@mui/material/Paper";
+import Stepper from "@mui/material/Stepper";
+import Step from "@mui/material/Step";
+import StepLabel from "@mui/material/StepLabel";
+import Stack from "@mui/material/Stack";
+import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 
 import NavBar from "./NavBar";
+import Footer from "./Footer";
+
+function getSteps() {
+  return ["Shipping address", "Review your order"];
+}
+
+function getStepContent(stepIndex: number) {
+  switch (stepIndex) {
+    case 0:
+      return "Here it goes the solid pod's thing";
+    case 1:
+      return "Review your order";
+  }
+}
 
 export default function Checkout() {
+  const [activeStep, setActiveStep] = React.useState(0);
+  const steps = getSteps();
+
+  const handleNext = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep + 1);
+  };
+
+  const handleBack = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep - 1);
+  };
+
   return (
     <React.Fragment>
       <NavBar />
@@ -18,8 +48,37 @@ export default function Checkout() {
           <Typography component="h1" variant="h4" align="center">
             Checkout
           </Typography>
+          <Stepper activeStep={activeStep} alternativeLabel>
+            {steps.map((label) => (
+              <Step key={label}>
+                <StepLabel>{label}</StepLabel>
+              </Step>
+            ))}
+          </Stepper>
+
+          <Typography>{getStepContent(activeStep)}</Typography>
+
+          <Stack
+            direction={{ xs: "column", sm: "row" }}
+            justifyContent="space-between"
+            alignItems="center"
+          >
+            <Button
+              disabled={activeStep === 0}
+              onClick={handleBack}
+              variant="outlined"
+              className="m-1"
+            >
+              Back
+            </Button>
+
+            <Button variant="contained" onClick={handleNext} className="m-1">
+              {activeStep === steps.length - 1 ? "Finish" : "Next"}
+            </Button>
+          </Stack>
         </Paper>
       </Container>
+      <Footer />
     </React.Fragment>
   );
 }
