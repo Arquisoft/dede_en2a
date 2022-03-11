1c1594d9ed7152ddc7528f2d9284791b47303016
Alvaro Rodriguez Gonzalez <uo277776@uniovi.es>
2022-03-11 16:08:50 +0100

    Fixing merging errors

--- webapp/src/components/NavBar.tsx
+++ webapp/src/components/NavBar.tsx
@@ -22,8 +22,8 @@ import MenuList from "@mui/material/MenuList";
 import MenuItem from "@mui/material/MenuItem";
 import ListItemIcon from "@mui/material/ListItemIcon";
 
-import {Grid} from "@mui/material";
-import MUISwitch from "./ThemeSlider"
+import { Grid } from "@mui/material";
+import MUISwitch from "./ThemeSlider";
 
 type LogOutFuncProps = {
   logCurrentUserOut: () => void;
@@ -113,43 +113,43 @@ function UserButton(props: any): JSX.Element {
 }
 
 type NavBarProps = {
-    totalUnitsInCart: number,
-    logCurrentUserOut: () => void;
-    changeTheme: Function;
-    initialState: boolean;
+  totalUnitsInCart: number;
+  logCurrentUserOut: () => void;
+  changeTheme: Function;
+  initialState: boolean;
 };
 
 function NavBar(props: NavBarProps): JSX.Element {
-    const [state, setState] = React.useState(false);
+  const [state, setState] = React.useState(false);
 
-    const toggleDrawer =
-        (open: boolean) => (event: React.KeyboardEvent | React.MouseEvent) => {
-            if (
-                event &&
-                event.type === "keydown" &&
-                ((event as React.KeyboardEvent).key === "Tab" ||
-                    (event as React.KeyboardEvent).key === "Shift")
-            )
-                return;
+  const toggleDrawer =
+    (open: boolean) => (event: React.KeyboardEvent | React.MouseEvent) => {
+      if (
+        event &&
+        event.type === "keydown" &&
+        ((event as React.KeyboardEvent).key === "Tab" ||
+          (event as React.KeyboardEvent).key === "Shift")
+      )
+        return;
 
-            setState(open);
-        };
+      setState(open);
+    };
 
-    const list = () => (
-        <Grid
-            container
-            alignItems="center"
-            direction="column"
-            onClick={toggleDrawer(false)}
-            onKeyDown={toggleDrawer(false)}
-        >
-            <MenuList>
-                <MenuItem component={Link} to="/">
-                    <ListItemIcon>
-                        <HomeIcon fontSize="small"/>
-                    </ListItemIcon>
-                    <Typography variant="inherit">Home</Typography>
-                </MenuItem>
+  const list = () => (
+    <Grid
+      container
+      alignItems="center"
+      direction="column"
+      onClick={toggleDrawer(false)}
+      onKeyDown={toggleDrawer(false)}
+    >
+      <MenuList>
+        <MenuItem component={Link} to="/">
+          <ListItemIcon>
+            <HomeIcon fontSize="small" />
+          </ListItemIcon>
+          <Typography variant="inherit">Home</Typography>
+        </MenuItem>
 
         <MenuItem component={Link} to="/cart">
           <ListItemIcon>
@@ -161,72 +161,70 @@ function NavBar(props: NavBarProps): JSX.Element {
     </Grid>
   );
 
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            onClick={toggleDrawer(true)}
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <SwipeableDrawer
+            anchor="left"
+            open={state}
+            onClose={toggleDrawer(false)}
+            onOpen={toggleDrawer(true)}
+          >
+            {list()}
+          </SwipeableDrawer>
 
-    return (
-        <Box sx={{flexGrow: 1}}>
-            <AppBar position="static">
-                <Toolbar>
-                    <IconButton
-                        size="large"
-                        edge="start"
-                        color="inherit"
-                        aria-label="menu"
-                        onClick={toggleDrawer(true)}
-                        sx={{mr: 2}}
-                    >
-                        <MenuIcon/>
-                    </IconButton>
-                    <SwipeableDrawer
-                        anchor="left"
-                        open={state}
-                        onClose={toggleDrawer(false)}
-                        onOpen={toggleDrawer(true)}
-                    >
-                        {list()}
-                    </SwipeableDrawer>
+          <Typography
+            variant="h6"
+            component={Link}
+            to="/"
+            color="inherit"
+            sx={{
+              flexGrow: 1,
+              textDecoration: "none",
+              ":hover": {
+                color: "white",
+              },
+            }}
+          >
+            Dede
+          </Typography>
 
-                    <Typography
-                        variant="h6"
-                        component={Link}
-                        to="/"
-                        color="inherit"
-                        sx={{
-                            flexGrow: 1,
-                            textDecoration: "none",
-                            ":hover": {
-                                color: "white",
-                            },
-                        }}
-                    >
-                        Dede
-                    </Typography>
+          <Button onClick={() => props.changeTheme()}>
+            <MUISwitch
+              onChange={(e) => {
+                props.changeTheme();
+              }}
+              checked={props.initialState}
+            />
+          </Button>
 
-                    <Button onClick={() => props.changeTheme()}>
-                    <MUISwitch
-                        onChange={(e) => {
-                            props.changeTheme();
-                        }}
-                        checked={props.initialState}
-                    />
-                    </Button>
-
-                    <IconButton
-                        size="large"
-                        color="inherit"
-                        component={Link}
-                        to="/cart"
-                        sx={{mr: 2}}
-                    >
-                        <Badge badgeContent={props.totalUnitsInCart} color="error">
-                            <ShoppingCartIcon/>
-                        </Badge>
-                    </IconButton>
-                    <UserButton/>
-                    <LogOut logCurrentUserOut={props.logCurrentUserOut}/>
-                </Toolbar>
-            </AppBar>
-        </Box>
-    );
+          <IconButton
+            size="large"
+            color="inherit"
+            component={Link}
+            to="/cart"
+            sx={{ mr: 2 }}
+          >
+            <Badge badgeContent={props.totalUnitsInCart} color="error">
+              <ShoppingCartIcon />
+            </Badge>
+          </IconButton>
+          <UserButton />
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
 }
 
 export default NavBar;
