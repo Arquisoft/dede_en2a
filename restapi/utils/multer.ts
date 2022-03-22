5181ee8ca2ac84b25b582984f9bf38e419709ef2
Pablo <uo271580@uniovi.es>
2022-03-22 23:16:49 +0100

    Hotspot correct ya?!

--- restapi/utils/multer.ts
+++ restapi/utils/multer.ts
@@ -1,14 +1,12 @@
 import multer from "multer";
 
-const storage = multer.diskStorage({
-  destination: "../webapp/src/images",
-  filename: (req, file, cb) => {
-    cb(null, file.originalname.toLowerCase()); //+ path.extname(file.originalname).toLowerCase()
-  },
-});
-
 export default multer({
-  storage: storage,
+  storage: multer.diskStorage({
+    destination: "../webapp/src/images",
+    filename: (req, file, cb) => {
+      cb(null, file.originalname.toLowerCase()); //+ path.extname(file.originalname).toLowerCase()
+    },
+  }),
   limits: {
     fileSize: 8000000,
   },
