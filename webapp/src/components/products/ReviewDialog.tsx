4f7aa20ce38d75a90ee3b24c4dfd28ad3b0b5122
Sitoo <uo277915@gmail.com>
2022-03-27 23:18:40 +0200

    Fixed the dialogs so they reuse the code.

--- webapp/src/components/products/ReviewDialog.tsx
+++ webapp/src/components/products/ReviewDialog.tsx
@@ -1,200 +1,131 @@
 import * as React from "react";
 
-import PropTypes from "prop-types";
-
-import Button from "@mui/material/Button";
-import { styled } from "@mui/material/styles";
-import Dialog from "@mui/material/Dialog";
-import DialogTitle from "@mui/material/DialogTitle";
-import DialogContent from "@mui/material/DialogContent";
-import DialogActions from "@mui/material/DialogActions";
 import IconButton from "@mui/material/IconButton";
 import CloseIcon from "@mui/icons-material/Close";
 import Typography from "@mui/material/Typography";
 import Rating from "@mui/material/Rating";
-import { Alert, Divider, Snackbar, TextField } from "@mui/material";
-import SendIcon from "@mui/icons-material/Send";
-import { AlertColor } from "@mui/material/Alert";
+import {Alert, Divider, Snackbar, TextField} from "@mui/material";
+import {AlertColor} from "@mui/material/Alert";
 
-import { Product, Review } from "../../shared/shareddtypes";
-import { getReviewsByCodeAndEmail, addReview } from "../../api/api";
+import {Product, Review} from "../../shared/shareddtypes";
+import {addReview, getReviewsByCodeAndEmail} from "../../api/api";
+import DialogWrapper from "../dialogs/Dialog";
 
-const BootstrapDialog = styled(Dialog)(({ theme }) => ({
-  "& .MuiDialogContent-root": {
-    padding: theme.spacing(2),
-  },
-  "& .MuiDialogActions-root": {
-    padding: theme.spacing(1),
-  },
-}));
-
-type bdtProps = {
-  children: any;
-  onClose: any;
-};
-
-const BootstrapDialogTitle = (props: bdtProps) => {
-  const { children, onClose, ...other } = props;
-
-  return (
-    <DialogTitle sx={{ m: 0, p: 2 }} {...other}>
-      {children}
-      {onClose ? (
-        <IconButton
-          aria-label="close"
-          onClick={onClose}
-          sx={{
-            position: "absolute",
-            right: 8,
-            top: 8,
-            color: (theme) => theme.palette.grey[500],
-          }}
-        >
-          <CloseIcon />
-        </IconButton>
-      ) : null}
-    </DialogTitle>
-  );
-};
-
-BootstrapDialogTitle.propTypes = {
-  children: PropTypes.node,
-  onClose: PropTypes.func.isRequired,
-};
 type ReviewDialogProps = {
-  product: Product;
-  initialValue?: number;
-  show: number;
-  stars: number;
+    product: Product;
+    initialValue?: number;
+    show: number;
+    stars: number;
 };
 
 export default function ReviewDialog(props: ReviewDialogProps) {
-  const [open, setOpen] = React.useState(false);
-  const [show, showInfo] = React.useState(false);
-  const [btnDisabled, setBtnDisabled] = React.useState(false);
-  const [rating, setRating] = React.useState(0);
-  const [comment, setComment] = React.useState("");
+    const [open, setOpen] = React.useState(0);
+    const [close, setClose] = React.useState(0);
+    const [show, showInfo] = React.useState(false);
+    const [btnDisabled, setBtnDisabled] = React.useState(false);
+    const [rating, setRating] = React.useState(0);
+    const [comment, setComment] = React.useState("");
 
-  const [infoMessage, setInfoMessage] = React.useState("");
-  const [infoType, setInfoType] = React.useState<AlertColor>("success");
+    const [infoMessage, setInfoMessage] = React.useState("");
+    const [infoType, setInfoType] = React.useState<AlertColor>("success");
 
-  React.useEffect(() => {
-    setOpen(props.show > 0);
-  }, [props.show]);
+    React.useEffect(() => {
+        setRating(props.stars);
+        if (props.show > 1)
+            setOpen(props.show + 1);
+    }, [props.stars]);
 
-  React.useEffect(() => {
-    setRating(props.stars);
-    setOpen(props.show > 0);
-  }, [props.stars]);
+    const handleCloseInfo = () => {
+        showInfo(false);
+    };
 
-  const handleClose = () => {
-    setOpen(false);
-  };
-  const handleCloseInfo = () => {
-    showInfo(false);
-  };
+    const showMessage = (infotype: AlertColor, infomessage: string) => {
+        setInfoMessage(infomessage);
+        setInfoType(infotype);
+        showInfo(true);
+    };
 
-  const showMessage = (infotype: AlertColor, infomessage: string) => {
-    setInfoMessage(infomessage);
-    setInfoType(infotype);
-    showInfo(true);
-  };
+    const handleConfirm = async () => {
+        if (btnDisabled) {
+            showMessage("error", "You must complete the forms!");
+            return;
+        }
 
-  const handleConfirm = async () => {
-    setOpen(false);
+        setClose(close+1);
 
-    let userEmail = localStorage.getItem("user.email");
+        let userEmail = localStorage.getItem("user.email");
 
-    if (userEmail == null) {
-      showMessage("error", "You must log in first!");
-    } else if (
-      (await getReviewsByCodeAndEmail(props.product.code, userEmail)).length > 0
-    ) {
-      showMessage("error", "You have already reviewed this product!");
-    } else {
-      let createdReview: Review = {
-        rating: rating,
-        comment: comment,
-        userEmail: userEmail,
-        productCode: props.product.code,
-      };
-      let addStatus: boolean = await addReview(createdReview);
+        if (userEmail == null) {
+            showMessage("error", "You must log in first!");
+        } else if (
+            (await getReviewsByCodeAndEmail(props.product.code, userEmail)).length > 0
+        ) {
+            showMessage("error", "You have already reviewed this product!");
+        } else {
+            let createdReview: Review = {
+                rating: rating,
+                comment: comment,
+                userEmail: userEmail,
+                productCode: props.product.code,
+            };
+            let addStatus: boolean = await addReview(createdReview);
 
-      if (addStatus) {
-        showMessage("success", "Review added correctly!");
-        window.location.reload();
-      } else {
-        showMessage("error", "There was an error creating your review!");
-      }
-    }
-  };
+            if (addStatus) {
+                showMessage("success", "Review added correctly!");
+                window.location.reload();
+            } else {
+                showMessage("error", "There was an error creating your review!");
+            }
+        }
+    };
 
-  return (
-    <React.Fragment>
-      <BootstrapDialog
-        onClose={handleClose}
-        aria-labelledby="customized-dialog-title"
-        open={open}
-      >
-        <BootstrapDialogTitle onClose={handleClose}>
-          Rate this product!
-        </BootstrapDialogTitle>
-        <DialogContent dividers style={{ minWidth: "30vw" }}>
-          <Typography> Your rating is: </Typography>
-          <Rating
-            value={rating}
-            onChange={(event, newValue) => {
-              if (newValue != null) {
-                setRating(newValue);
-                setBtnDisabled(false);
-              }
-            }}
-            size="large"
-            precision={0.5}
-          />
-          <Divider />
-          <TextField
-            autoFocus
-            id="outlined-basic"
-            label="Comment"
-            multiline
-            variant="outlined"
-            onChange={(event) => {
-              setComment(event.target.value);
-            }}
-            style={{ margin: "2vh .2vw" }}
-            sx={{ width: "100%" }}
-            minRows={5}
-          />
-        </DialogContent>
-        <DialogActions>
-          <Button
-            variant="contained"
-            endIcon={<SendIcon />}
-            onClick={handleConfirm}
-            disabled={btnDisabled}
-          >
-            Send Rating
-          </Button>
-        </DialogActions>
-      </BootstrapDialog>
-      <Snackbar open={show} autoHideDuration={6000} onClose={handleCloseInfo}>
-        <Alert
-          severity={infoType}
-          action={
-            <IconButton
-              size="small"
-              aria-label="close"
-              color="inherit"
-              onClick={handleCloseInfo}
-            >
-              <CloseIcon fontSize="small" />
-            </IconButton>
-          }
-        >
-          {infoMessage}
-        </Alert>
-      </Snackbar>
-    </React.Fragment>
-  );
+    return (
+        <React.Fragment>
+            <DialogWrapper show={open} hide={close} titleText="Rate this product" handleConfirm={handleConfirm}>
+                <Typography> Your rating is: </Typography>
+                <Rating
+                    value={rating}
+                    onChange={(event, newValue) => {
+                        if (newValue != null) {
+                            setRating(newValue);
+                            setBtnDisabled(false);
+                        }
+                    }}
+                    size="large"
+                    precision={0.5}
+                />
+                <Divider/>
+                <TextField
+                    autoFocus
+                    id="outlined-basic"
+                    label="Comment"
+                    multiline
+                    variant="outlined"
+                    onChange={(event) => {
+                        setComment(event.target.value);
+                    }}
+                    style={{margin: "2vh .2vw"}}
+                    sx={{width: "100%"}}
+                    minRows={5}
+                />
+            </DialogWrapper>
+            <Snackbar open={show} autoHideDuration={6000} onClose={handleCloseInfo}>
+                <Alert
+                    severity={infoType}
+                    action={
+                        <IconButton
+                            size="small"
+                            aria-label="close"
+                            color="inherit"
+                            onClick={handleCloseInfo}
+                        >
+                            <CloseIcon fontSize="small"/>
+                        </IconButton>
+                    }
+                >
+                    {infoMessage}
+                </Alert>
+            </Snackbar>
+        </React.Fragment>
+    );
 }
