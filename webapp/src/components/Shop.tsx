74cb17d572cfb5600f6e7c4dab7e660a16c3ede6
Pablo <uo271580@uniovi.es>
2022-03-28 12:35:22 +0200

    Solving pagination issues

--- webapp/src/components/Shop.tsx
+++ webapp/src/components/Shop.tsx
@@ -1,13 +1,15 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 
-import Typography from "@mui/material/Typography";
+import {
+  styled,
+  Stack,
+  Typography,
+  Pagination,
+  TextField,
+} from "@mui/material";
 
 import { CartItem, Product } from "../shared/shareddtypes";
-
 import ProductList from "./products/ProductList";
-import { alpha, styled, TextField } from "@mui/material";
-import SearchIcon from '@mui/icons-material/Search';
-
 
 type HomeProps = {
   products: Product[];
@@ -15,19 +17,28 @@ type HomeProps = {
   onAdd: (product: Product) => void;
 };
 
-const Search = styled('div')({
+const Search = styled("div")({
   marginLeft: "30px",
 });
 
-function Home(props: HomeProps): JSX.Element {
-
+export default function Shop(props: HomeProps): JSX.Element {
+  let products: Product[] = [];
   const [searchTerm, setSearchTerm] = useState("");
-  let productos : Product[] = [];
-  
+  const [page, setPage] = React.useState(1);
+  const [itemsPerPage] = React.useState(12);
+
+  const handleChange = (event: React.ChangeEvent<unknown>, value: number) => {
+    setPage(value);
+  };
+
+  const obtainNumberOfPages = () => {
+    return props.products.length % itemsPerPage === 0
+      ? Math.floor(props.products.length / itemsPerPage)
+      : Math.floor(props.products.length / itemsPerPage) + 1;
+  };
 
   return (
     <React.Fragment>
-      <div>
       <Typography
         component="h1"
         variant="h4"
@@ -36,24 +47,41 @@ function Home(props: HomeProps): JSX.Element {
       >
         Shop
       </Typography>
+
       <Search>
-        <TextField type="text" id="search" label="Search..." variant="standard" onChange={event => {setSearchTerm(event.target.value)}}/>
+        <TextField
+          type="text"
+          id="search"
+          label="Search..."
+          variant="standard"
+          onChange={(event) => setSearchTerm(event.target.value)}
+        />
       </Search>
-      </div>
+
       {props.products.filter((val) => {
-        if(searchTerm == ""){
-          productos = props.products;
-        }else if(val.name.toLowerCase().includes(searchTerm.toLowerCase())){
-          productos.push(val);
-        }
+        if (searchTerm == "") products = props.products;
+        else if (val.name.toLowerCase().includes(searchTerm.toLowerCase()))
+          products.push(val);
       })}
-      <ProductList
-          products={productos}
+
+      <Stack justifyContent="center" alignItems="center" spacing={2}>
+        <ProductList
           cartProducts={props.cartProducts}
           OnAddCart={props.onAdd}
-      />
+          products={products.slice(
+            (page - 1) * itemsPerPage,
+            (page - 1) * itemsPerPage + itemsPerPage
+          )}
+        />
+
+        <Pagination
+          count={obtainNumberOfPages()}
+          page={page}
+          onChange={handleChange}
+          showFirstButton
+          showLastButton
+        />
+      </Stack>
     </React.Fragment>
   );
 }
-
-export default Home;
