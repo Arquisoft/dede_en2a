ccfc8490033b0f3175a4f7ca570b397ce81de088
Alvaro Rodriguez Gonzalez <UO277776@uniovi.es>
2022-03-29 12:10:13 +0200

    StockAlert tests and refactoring ProductBox tests

--- webapp/src/tests/ProductBox.test.tsx
+++ webapp/src/tests/ProductBox.test.tsx
@@ -3,9 +3,8 @@ import { fireEvent, render } from "@testing-library/react";
 import ProductBox from "../components/products/ProductBox";
 import { Product, CartItem } from "../shared/shareddtypes";
 import { BrowserRouter as Router } from "react-router-dom";
-import { createEmitAndSemanticDiagnosticsBuilderProgram } from "typescript";
 
-test("A product is rendered with stock > 10", async () => {
+test("A product is rendered", async () => {
   const product: Product = {
     code: "9999",
     name: "Producto Prueba 1",
@@ -31,62 +30,6 @@ test("A product is rendered with stock > 10", async () => {
 
   //Check that the info about stock is rendered correctly
   expect(getByText("Stock available!")).toBeInTheDocument();
-});
-
-test("A product is rendered with stock < 10", async () => {
-  const product: Product = {
-    code: "9999",
-    name: "Producto Prueba 1",
-    description: "Descripcion Prueba 1",
-    price: 10,
-    stock: 5,
-    image: "",
-  };
-
-  const { getByText, container } = render(
-    <Router>
-      <ProductBox product={product} currentCartAmount={0} onAdd={() => {}} />
-    </Router>
-  );
-
-  expect(getByText(product.name)).toBeInTheDocument();
-  expect(getByText(product.price + "€")).toBeInTheDocument();
-  expect(getByText(product.description)).toBeInTheDocument();
-
-  //Check that the default image is rendered
-  const image = container.querySelector("img") as HTMLImageElement;
-  expect(image.src).toContain("not-found.png");
-
-  //Check that the info about stock is rendered correctly
-  expect(getByText("Few units left!")).toBeInTheDocument();
-});
-
-test("A product is rendered with stock == 10", async () => {
-  const product: Product = {
-    code: "9999",
-    name: "Producto Prueba 1",
-    description: "Descripcion Prueba 1",
-    price: 10,
-    stock: 0,
-    image: "",
-  };
-
-  const { getByText, container } = render(
-    <Router>
-      <ProductBox product={product} currentCartAmount={0} onAdd={() => {}} />
-    </Router>
-  );
-
-  expect(getByText(product.name)).toBeInTheDocument();
-  expect(getByText(product.price + "€")).toBeInTheDocument();
-  expect(getByText(product.description)).toBeInTheDocument();
-
-  //Check that the default image is rendered
-  const image = container.querySelector("img") as HTMLImageElement;
-  expect(image.src).toContain("not-found.png");
-
-  //Check that the info about stock is rendered correctly
-  expect(getByText("No stock available!")).toBeInTheDocument();
 });
 
 test("A product is added to the cart", async () => {
@@ -115,4 +58,26 @@ test("A product is added to the cart", async () => {
   const addButton = getByText("Add product");
   fireEvent.click(addButton);
   expect(cart.length).toEqual(1);
+});
+
+test("URL to product details works", async () => {
+  const product: Product = {
+    code: "9999",
+    name: "Producto Prueba 1",
+    description: "Descripcion Prueba 1",
+    price: 10,
+    stock: 20,
+    image: "",
+  };
+  const cart: CartItem[] = [];
+
+  const { container } = render(
+    <Router>
+      <ProductBox product={product} currentCartAmount={0} onAdd={() => {}} />
+    </Router>
+  );
+
+  const hrefDetails = container.querySelector("img") as HTMLElement;
+  fireEvent.click(hrefDetails);
+  expect(window.location.pathname).toEqual("/product/" + product.code);
 });
