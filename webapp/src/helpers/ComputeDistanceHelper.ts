3b2f1c093df69eb4159be9cfe115dfeb26a12634
Pablo <uo271580@uniovi.es>
2022-03-30 16:08:05 +0200

    Add nearby places search

--- webapp/src/helpers/ComputeDistanceHelper.ts
+++ webapp/src/helpers/ComputeDistanceHelper.ts
@@ -1,3 +1,5 @@
+import { getPlaces } from "../api/api";
+
 const fromCoords: String = "43.35513026876176, -5.851290035687373"; //Coordinates of EII
 
 export const calculateShippingCosts = async (destAddress: String) => {
@@ -31,7 +33,8 @@ function calculateCoordinates(address: String) {
     .get(
       "https://api.mapbox.com/geocoding/v5/mapbox.places/" +
         address +
-        ".json?access_token=" + process.env.REACT_APP_MAPBOX_KEY
+        ".json?access_token=" +
+        process.env.REACT_APP_MAPBOX_KEY
     )
     .then((response: any) => {
       return response.data;
@@ -50,7 +53,8 @@ function getDistanceDriving(destCoords: String) {
         fromCoords +
         "&wp.1=" +
         destCoords +
-        "&key=" + process.env.REACT_APP_BING_KEY
+        "&key=" +
+        process.env.REACT_APP_BING_KEY
     )
     .then((response: any) => {
       return response.data.resourceSets[0].resources[0].travelDistance;
@@ -66,7 +70,8 @@ function getRouteImage(destCoords: String) {
         fromCoords +
         "&wp.1=" +
         destCoords +
-        "&key=" + process.env.REACT_APP_BING_KEY,
+        "&key=" +
+        process.env.REACT_APP_BING_KEY,
       { responseType: "blob" }
     )
     .then((response: any) => {
@@ -74,29 +79,11 @@ function getRouteImage(destCoords: String) {
     });
 }
 
-//Use to calculate the distance in straight line
-/*
-export function getDistanceFromLatLonInKm(
-    lat1: number,
-    lon1: number,
-    lat2: number,
-    lon2: number
-  ) {
-    var R = 6371; // Radius of the earth in km
-    var dLat = deg2rad(lat2 - lat1); // deg2rad below
-    var dLon = deg2rad(lon2 - lon1);
-    var a =
-      Math.sin(dLat / 2) * Math.sin(dLat / 2) +
-      Math.cos(deg2rad(lat1)) *
-        Math.cos(deg2rad(lat2)) *
-        Math.sin(dLon / 2) *
-        Math.sin(dLon / 2);
-    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-    var d = R * c; // Distance in km
-    return d;
-  }
-  
-function deg2rad(deg: number) {
-    return deg * (Math.PI / 180);
-  }
-*/
+export async function getNearByPlaces(address: String, radiusMeters: number, maxResults: number) {
+  let coords = await calculateCoordinates(address);
+  const x = coords.features[0].geometry.coordinates[0];
+  const y = coords.features[0].geometry.coordinates[1];
+
+  const places = await getPlaces(x, y, radiusMeters, maxResults);
+  console.log(places);
+}
