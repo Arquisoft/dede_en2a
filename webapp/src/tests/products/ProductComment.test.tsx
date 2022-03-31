649a451e0cfcd7cfad767f59c8b05d8020d4a66a
Alvaro Rodriguez Gonzalez <uo277776@uniovi.es>
2022-03-31 18:13:22 +0200

    Product details, comments and list tests

---
+++ webapp/src/tests/products/ProductComment.test.tsx
@@ -0,0 +1,22 @@
+import { render } from "@testing-library/react";
+import ProductComment from "../../components/products/ProductComment";
+import { Review } from "../../shared/shareddtypes";
+
+//Test that a review is rendered correctly
+test("Renders a review", () => {
+  const review: Review = {
+    userEmail: "test@email.com",
+    productCode: "9999",
+    rating: 4,
+    comment: "This is a test comment",
+  };
+
+  const { getByText, container } = render(<ProductComment review={review} />);
+
+  expect(getByText(review.userEmail)).toBeInTheDocument();
+  expect(getByText(review.comment)).toBeInTheDocument();
+
+  //Check that the rating is rendered correctly with the correct number of stars with aria-label = "4 Stars"
+  const rating = container.querySelector("[aria-label='4 Stars']");
+  expect(rating).toBeInTheDocument();
+});
