f96312ad68d594f827c79b8e4572f27904577f92
Sitoo <uo277915@gmail.com>
2022-04-02 19:04:36 +0200

    Tests created and checked

--- webapp/src/components/products/ProductSpeedDial.tsx
+++ webapp/src/components/products/ProductSpeedDial.tsx
@@ -22,12 +22,13 @@ export default function ProductSpeedDial(props: ProductSpeedDialProps) {
 
     return (
         <SpeedDial
-            ariaLabel="SpeedDial basic example"
+            ariaLabel="SpeedDial"
             sx={{position: "fixed", bottom: 16, right: 16}}
             icon={<SpeedDialIcon/>}
         >
             {actions.map((action) => (
                 <SpeedDialAction
+                    aria-label={action.name}
                     key={action.name}
                     icon={action.icon}
                     tooltipTitle={action.name}
---
+++ webapp/src/tests/products/ProductSpeedDial.test.tsx
@@ -0,0 +1,80 @@
+import React from "react";
+
+import { fireEvent, render, screen } from "@testing-library/react";
+import ProductSpeedDial from "../../components/products/ProductSpeedDial";
+import { Product, CartItem } from "../../shared/shareddtypes";
+import { BrowserRouter as Router } from "react-router-dom";
+import ProductBox from "../../components/products/ProductBox";
+import {SpeedDialAction} from "@mui/material";
+
+const empty = () => {};
+
+test("The dial is rendered", async () => {
+
+    const { getByText, container } = render(
+        <Router>
+            <ProductSpeedDial addToCart={empty} review={empty} share={empty}/>
+        </Router>
+    );
+
+    let addButton = container.querySelector("[aria-label='Add to Shopping Cart']");
+    expect(addButton).toBeInTheDocument();
+
+    let reviewButton = container.querySelector("[aria-label='Review item']");
+    expect(reviewButton).toBeInTheDocument();
+
+    let shareButton = container.querySelector("[aria-label='Share']");
+    expect(shareButton).toBeInTheDocument();
+
+});
+
+test("The Add Button Works", async () => {
+
+    let works = false;
+    const buttonTest = () => {works = true;};
+
+    const { getByText, container } = render(
+        <Router>
+            <ProductSpeedDial addToCart={buttonTest} review={empty} share={empty}/>
+        </Router>
+    );
+
+    let addButton = container.querySelector("[aria-label='Add to Shopping Cart']") as HTMLElement;
+    fireEvent.click(addButton);
+
+    expect(works)
+});
+
+test("The Review Button Works", async () => {
+
+    let works = false;
+    const buttonTest = () => {works = true;};
+
+    const { getByText, container } = render(
+        <Router>
+            <ProductSpeedDial addToCart={empty} review={buttonTest} share={empty}/>
+        </Router>
+    );
+
+    let addButton = container.querySelector("[aria-label='Review item']") as HTMLElement;
+    fireEvent.click(addButton);
+
+    expect(works)
+});
+
+test("The Share Button Works", async () => {
+
+    let works = false;
+    const buttonTest = () => {works = true;};
+
+    const { getByText, container } = render(
+        <Router>
+            <ProductSpeedDial addToCart={empty} review={empty} share={buttonTest}/>
+        </Router>
+    );
+
+    let addButton = container.querySelector("[aria-label='Share']") as HTMLElement;
+    fireEvent.click(addButton);
+
+    expect(works)
+});
