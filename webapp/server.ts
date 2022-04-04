9da45791f55ed4ab49bb3fd623676163291bc9b6
Alvaro Rodriguez Gonzalez <UO277776@uniovi.es>
2022-04-04 11:50:50 +0200

    Fixing cannot get error

--- webapp/server.ts
+++ webapp/server.ts
@@ -1,4 +1,6 @@
 import express,{Application} from 'express'; 
+const path = require('path');
+const publicPath = path.join(__dirname, '..', 'public');
 //for using an import here we need to configure the tsconfig.json
 //setting the option module to commonjs
 
@@ -6,6 +8,14 @@ var app: Application = express()
 const port: number = 3000;
 
 app.use(express.static('build'))
+
+app.get('*', (req, res) => {    
+    res.sendFile(path.join(publicPath, 'index.html')), function(err) {             
+    if (err) {                 
+         res.status(500).send(err) 
+         }        
+    };
+});
 
 app.listen(port, ():void => {
     console.log('Webapp started on port '+ port);
