5f29ec500667b3caff1ae1b8bf26841499197897
Pablo <uo271580@uniovi.es>
2022-04-04 20:24:19 +0200

    Review tests

--- restapi/reviews/ReviewController.ts
+++ restapi/reviews/ReviewController.ts
@@ -3,14 +3,10 @@ import { verifyToken } from "../utils/generateToken";
 import { reviewModel } from "./Review";
 
 export const getReviewsByProduct: RequestHandler = async (req, res) => {
-  try {
-    const reviews = await reviewModel.find({
-      productCode: req.params.productCode,
-    });
-    return res.json(reviews);
-  } catch (error) {
-    res.json(error);
-  }
+  const reviews = await reviewModel.find({
+    productCode: req.params.productCode,
+  });
+  return res.json(reviews);
 };
 
 export const createReview: RequestHandler = async (req, res) => {
@@ -21,21 +17,17 @@ export const createReview: RequestHandler = async (req, res) => {
       const reviewSaved = await review.save();
       res.json(reviewSaved);
     } catch (error) {
-      res.status(412).json({ message: "The data is not valid " + error });
+      res.status(412).json({ message: "The data is not valid" });
     }
   } else {
-    res.status(203).json({ message: "Invalid token " });
+    res.status(403).json({ message: "Invalid token" });
   }
 };
 
 export const getReviewsByProductAndUser: RequestHandler = async (req, res) => {
-  try {
-    const reviews = await reviewModel.find({
-      productCode: req.params.productCode,
-      userEmail: req.params.email,
-    });
-    return res.json(reviews);
-  } catch (error) {
-    res.json(error);
-  }
+  const reviews = await reviewModel.find({
+    productCode: req.params.productCode,
+    userEmail: req.params.email,
+  });
+  return res.json(reviews);
 };
