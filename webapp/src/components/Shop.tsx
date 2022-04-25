4973da0fae91d2fca0992877d8dced1665bbeb5e
noeliaigc <noeliaiglesiasc@gmail.com>
2022-04-25 11:16:32 +0200

    Return to page 1 when filter selected

--- webapp/src/components/Shop.tsx
+++ webapp/src/components/Shop.tsx
@@ -154,6 +154,7 @@ export default function Shop(props: HomeProps): JSX.Element {
     }
 
     setFilteredProducts(await filterProductsByCategory(category, orderPrice));
+    setPage(1);
 
     
   };
