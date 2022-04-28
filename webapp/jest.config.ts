a01a18a86f4f7e6d8deba848d5a3b3bac0147e9f
Pablo <uo271580@uniovi.es>
2022-04-28 13:25:56 +0200

    Modifying jest configuration webapp

--- webapp/jest.config.ts
+++ webapp/jest.config.ts
@@ -1,5 +1,8 @@
 export default {
-    transform: {
-        "^.+\\.tsx?$": "ts-jest"
-    },
-}
+  transform: {
+    "^.+\\.tsx?$": "ts-jest",
+  },
+  collectCoverage: true,
+  collectCoverageFrom: ["**/*.tsx"],
+  testTimeout: 15000,
+};
