d6231694a69b6786b43fecee51887acc99524428
Ángel Iglesias Préstamo <angel.iglesias.prestamo@gmail.com>
2022-05-01 16:21:17 +0200

    changing the background color of the sidebar dashboard

--- webapp/src/components/dashboard/misc/DashboardSideBar.tsx
+++ webapp/src/components/dashboard/misc/DashboardSideBar.tsx
@@ -17,38 +17,45 @@ type DashboardSideBarProps = {
 
 export default function DashboardSideBar(props: DashboardSideBarProps) {
   return (
-    <Paper
-      sx={{ position: "fixed", bottom: 0, left: 0, right: 0 }}
-      elevation={3}
+    <BottomNavigation
+      showLabels
+      sx={{
+        position: "fixed",
+        bottom: 0,
+        left: 0,
+        right: 0,
+        backgroundColor: (theme) =>
+          theme.palette.mode === "light"
+            ? theme.palette.grey[300]
+            : theme.palette.grey[800],
+      }}
     >
-      <BottomNavigation showLabels>
+      <BottomNavigationAction
+        component={Link}
+        to="/dashboard"
+        label="Dashboard"
+        icon={<SettingsIcon />}
+      />
+      <BottomNavigationAction
+        component={Link}
+        to="orders"
+        label="Orders"
+        icon={<MoveToInboxIcon />}
+      />
+      {isRenderForModeratorAtLeast(props.role) && (
         <BottomNavigationAction
           component={Link}
-          to="/dashboard"
-          label="Dashboard"
-          icon={<SettingsIcon />}
+          to="products"
+          label="Products"
+          icon={<LocalOfferIcon />}
         />
-        <BottomNavigationAction
-          component={Link}
-          to="orders"
-          label="Orders"
-          icon={<MoveToInboxIcon />}
-        />
-        {isRenderForModeratorAtLeast(props.role) && (
-          <BottomNavigationAction
-            component={Link}
-            to="products"
-            label="Products"
-            icon={<LocalOfferIcon />}
-          />
-        )}
-        <BottomNavigationAction
-          component={Link}
-          to="account"
-          label="Account Details"
-          icon={<ManageAccountsIcon />}
-        />
-      </BottomNavigation>
-    </Paper>
+      )}
+      <BottomNavigationAction
+        component={Link}
+        to="account"
+        label="Account Details"
+        icon={<ManageAccountsIcon />}
+      />
+    </BottomNavigation>
   );
 }
