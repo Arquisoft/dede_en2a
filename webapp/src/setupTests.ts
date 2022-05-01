99008234a8284509b62effbf2c190bc633dc52d0
Alvaro Rodriguez Gonzalez <uo277776@uniovi.es>
2022-05-01 18:20:40 +0200

    Fixing axios error

--- webapp/src/setupTests.ts
+++ webapp/src/setupTests.ts
@@ -3,4 +3,10 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import "@testing-library/jest-dom";
+import * as computeDistanceHelper from "./helpers/ComputeDistanceHelper";
 window.URL.createObjectURL = jest.fn();
+jest
+  .spyOn(computeDistanceHelper, "getCoordinatesFromAddress")
+  .mockImplementation((destAddress: String) => {
+    return Promise.resolve("36.23423,-5.23423");
+  });
